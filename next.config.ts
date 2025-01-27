f27d1d55c223a50dfe4551d5c954952698bfc78d
Alphenx <adriangarcia.comunicacion@gmail.com>
2025-01-27 18:58:47 +0100

    Update next config with enviroment variables

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    UPSTASH_URL: process.env.UPSTASH_URL,
+    UPSTASH_PASSWORD: process.env.UPSTASH_PASSWORD,
+  },
 };
 
 export default nextConfig;
