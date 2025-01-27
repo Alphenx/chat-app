2e450884c0b8baa6a6d993587861ea670ddc0138
Alphenx <adriangarcia.comunicacion@gmail.com>
2025-01-27 20:33:47 +0100

    Added `GOOGLE_CLIENT_ID` and `GOOGLE_CLIENT_SECRET` to next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     UPSTASH_URL: process.env.UPSTASH_URL,
     UPSTASH_PASSWORD: process.env.UPSTASH_PASSWORD,
   },
