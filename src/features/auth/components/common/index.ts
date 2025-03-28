dafe4988f0820ec7d06f4d9e07a92facc2ab9602
Alphenx <adriangarcia.comunicacion@gmail.com>
2025-03-28 19:40:40 +0100

    Add `FormTitle`, `FormContainer` and `FormAlternativeMethods`

---
+++ src/features/auth/components/common/index.ts
@@ -0,0 +1,4 @@
+export { default as FormAlternativeMethods } from './FormAlternativeMethods';
+export { default as FormContainer } from './FormContainer';
+export { default as FormFeedback } from './FormFeedback';
+export { default as FormTitle } from './FormTitle';
