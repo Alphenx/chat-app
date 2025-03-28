5bf68fccd14fdff00c80675d73be82f385e4225c
Alphenx <adriangarcia.comunicacion@gmail.com>
2025-03-28 12:20:20 +0100

    Add `UserRepository` and `User` entity model

--- src/features/user/models/user.d.ts
+++ src/features/user/models/user.d.ts
@@ -1,6 +1,27 @@
-interface User {
-  id: string;
+type UserId = string;
+
+interface BaseUser {
+  id: UserId;
   name: string;
   email: string;
-  image: string;
+  image?: string;
 }
+
+interface PrivateUser extends BaseUser {
+  password: string;
+  emailVerified: boolean;
+}
+
+type PublicUser = BaseUser;
+
+interface User extends PublicUser {
+  friends: UserId[];
+  friendRequests: UserId[];
+}
+
+type CreateInternalUserDTO = Omit<PrivateUser, 'id'>;
+type CreateExternalUserDTO = Omit<BaseUser, 'id'>;
+
+type CreateUserDTO = CreateInternalUserDTO | CreateExternalUserDTO;
+
+type UpdateUserDTO = Partial<CreateUserDTO>;
