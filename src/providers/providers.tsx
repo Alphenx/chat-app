9f4d7eadc2ab239d4f9bb7e0a76ee417372403cf
Alphenx <adriangarcia.comunicacion@gmail.com>
2025-07-25 20:23:35 +0200

    Update `Providers`

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,28 +1,28 @@
 'use client';
+
 import useClient from '@/features/common/hooks/useClient';
+import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { memo, ReactNode } from 'react';
-import { StylesProvider } from './styles-provider';
-import { TranslationsProvider } from './translations-provider';
+import { TranslationsProvider } from '../features/i18n/contexts/translations.context';
 
 type Props = {
   children: ReactNode;
+  locale: Locale;
+  session: Session | null;
 };
 
-function Providers({ children }: Readonly<Props>) {
+function Providers({ children, session, locale }: Readonly<Props>) {
   const isClient = useClient();
 
-  if (!isClient) {
-    return null;
-  }
+  if (!isClient) return null;
 
-  return (
-    <StylesProvider>
-      <TranslationsProvider>
-        <SessionProvider>{children}</SessionProvider>
-      </TranslationsProvider>
-    </StylesProvider>
-  );
+  const providers: ((node: ReactNode) => ReactNode)[] = [
+    (node) => <TranslationsProvider initialLocale={locale}>{node}</TranslationsProvider>,
+    (node) => <SessionProvider session={session}>{node}</SessionProvider>,
+  ];
+
+  return <>{providers.reduceRight((acc, wrap) => wrap(acc), children)}</>;
 }
 
 export default memo(Providers);
