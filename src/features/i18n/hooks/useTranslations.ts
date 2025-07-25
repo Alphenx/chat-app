ecfa1ae96d07c7f3d0ffc6d11977c977e3731a1f
Alphenx <adriangarcia.comunicacion@gmail.com>
2025-07-25 19:27:11 +0200

    Add `useTranslations` hook

---
+++ src/features/i18n/hooks/useTranslations.ts
@@ -0,0 +1,30 @@
+import { useTranslationsContext } from '@/features/i18n/contexts/translations.context';
+import { createTranslator } from '@/features/i18n/utils/create-translator';
+import { useEffect, useMemo } from 'react';
+
+export function useTranslations<N extends Namespace, Keys extends readonly string[] = []>(
+  namespace: N,
+  ...keys: Keys
+): {
+  t: TranslatorOf<N, Keys>;
+} {
+  const { locale, translations, loadNamespace } = useTranslationsContext();
+
+  const nsTranslations = translations[namespace];
+
+  useEffect(() => {
+    if (!nsTranslations) {
+      loadNamespace(namespace);
+    }
+  }, [namespace, locale, nsTranslations, loadNamespace]);
+
+  const t = useMemo(() => {
+    if (!nsTranslations) {
+      return (fallback: string) => fallback;
+    }
+
+    return createTranslator(nsTranslations, namespace, ...keys);
+  }, [nsTranslations, keys, namespace]);
+
+  return { t };
+}
