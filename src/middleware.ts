237cd41c28c06bf0fb71bb6922aad10102a1d2b9
Alphenx <adriangarcia.comunicacion@gmail.com>
2025-07-27 16:41:52 +0200

    Add `middleware` and `routes` constants

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,40 @@
+import { AppRoute, PUBLIC_ROUTES, STATIC_PATHS } from '@/features/common/constants/routes';
+import type { NextRequestWithAuth } from 'next-auth/middleware';
 import { withAuth } from 'next-auth/middleware';
 import { NextResponse } from 'next/server';
 
 export default withAuth(
-  function middleware() {
+  function middleware(req: NextRequestWithAuth) {
+    const { pathname } = req.nextUrl;
+    const token = req.nextauth.token;
+    const isAuthenticated = !!token;
+
+    if (isStaticPath(pathname)) return NextResponse.next();
+
+    if (isAuthenticated && isPublicRoute(pathname)) {
+      return NextResponse.redirect(new URL(AppRoute.DASHBOARD, req.url));
+    }
+
     return NextResponse.next();
   },
   {
-    pages: {
-      signIn: '/login',
-      error: '',
-    },
     callbacks: {
-      authorized: ({ token }) => {
-        return !!token;
+      authorized: ({ token, req }) => {
+        const pathname = req.nextUrl.pathname;
+        return isStaticPath(pathname) || isPublicRoute(pathname) || !!token;
       },
     },
   }
 );
 
+function isPublicRoute(pathname: string) {
+  return PUBLIC_ROUTES.some((route) => pathname.startsWith(route));
+}
+
+function isStaticPath(pathname: string) {
+  return STATIC_PATHS.some((path) => pathname.startsWith(path));
+}
+
 export const config = {
-  matcher: ['/dashboard/:path*'],
+  matcher: ['/((?!api|_next|assets|favicon.ico|robots.txt|sitemap.xml).*)'],
 };
