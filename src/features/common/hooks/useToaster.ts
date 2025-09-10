0e415e9b9a7c97ff03b2a2237d658f6a18df7b75
Alphenx <adriangarcia.comunicacion@gmail.com>
2025-09-10 16:01:47 +0200

    export named useToaster

--- src/features/common/hooks/useToaster.ts
+++ src/features/common/hooks/useToaster.ts
@@ -1,7 +1,7 @@
 import { ToasterContext, ToasterContextReturn } from '@/providers/toaster-provider';
 import { useContext } from 'react';
 
-function useToaster(): ToasterContextReturn {
+export function useToaster(): ToasterContextReturn {
   const context = useContext(ToasterContext);
   if (!context) {
     throw new Error('useToaster must be used within a ToasterProvider');
