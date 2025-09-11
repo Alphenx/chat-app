7a207758445688c3f8661d62522b37eaf711bde7
Alphenx <adriangarcia.comunicacion@gmail.com>
2025-09-11 21:04:13 +0200

    Creation of `FriendRequestList` with "empty", "loading" and "error" state

---
+++ src/features/friends/components/FriendRequestList/FriendRequestList.empty.tsx
@@ -0,0 +1,25 @@
+import { SectionContainer, SectionHeader } from '@/components';
+import { Center, Icon, Text } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+import { FaUserFriends } from 'react-icons/fa';
+
+interface FriendRequestListEmptyProps {
+  title: ReactNode;
+  message: ReactNode;
+}
+
+function FriendRequestListEmpty({ title, message }: FriendRequestListEmptyProps) {
+  return (
+    <SectionContainer>
+      <SectionHeader title={title} />
+      <Center flexDirection='column' justifyContent='center' alignItems='center' gap='2' py='2'>
+        <Icon as={FaUserFriends} size='2xl' color='fg.muted' />
+        <Text color='fg.muted' textAlign='center' fontSize={{ base: 'sm', smDown: 'xs' }}>
+          {message}
+        </Text>
+      </Center>
+    </SectionContainer>
+  );
+}
+
+export default FriendRequestListEmpty;
