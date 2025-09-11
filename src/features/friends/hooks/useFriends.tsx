2ede3e84627473c1e20396e147faf76a92b543cf
Alphenx <adriangarcia.comunicacion@gmail.com>
2025-09-11 11:02:48 +0200

    Disable start chat action for now

--- src/features/friends/hooks/useFriends.tsx
+++ src/features/friends/hooks/useFriends.tsx
@@ -1,10 +1,8 @@
 import { useConfirmModal } from '@/features/common/hooks/useConfirmModal';
 import RemoveFriendConfirmModal from '@/features/friends/components/RemoveFriend/RemoveFriendConfirmModal';
 import { useFriendsActions, useFriendsState } from '@/features/friends/context/friends.context';
-import { useStartChatAction } from '@/features/friends/hooks/useStartChatAction';
 import { useTranslations } from '@/features/i18n/hooks/useTranslations';
 import { useMemo } from 'react';
-import { IoChatbubblesOutline } from 'react-icons/io5';
 import { MdDelete } from 'react-icons/md';
 
 export function useFriends() {
@@ -12,7 +10,7 @@ export function useFriends() {
 
   const { friends, loading, error, refresh } = useFriendsState();
   const { onRemoveFriend } = useFriendsActions();
-  const { prepareChatOnHover, startChatOnClick } = useStartChatAction();
+  // const { prepareChatOnHover, startChatOnClick } = useStartChatAction();
 
   const openRemoveFriendConfirmModal = useConfirmModal<PublicUser>({
     content: (props) => <RemoveFriendConfirmModal {...props} />,
@@ -21,13 +19,13 @@ export function useFriends() {
 
   const actions = useMemo(
     () => [
-      {
-        id: 'start-chat',
-        icon: <IoChatbubblesOutline />,
-        label: t('Start Chat', 'startChat.button'),
-        onClick: startChatOnClick,
-        onMouseEnter: prepareChatOnHover,
-      },
+      // {
+      //   id: 'start-chat',
+      //   icon: <IoChatbubblesOutline />,
+      //   label: t('Start Chat', 'startChat.button'),
+      //   onClick: startChatOnClick,
+      //   onMouseEnter: prepareChatOnHover,
+      // },
       {
         id: 'remove-friend',
         icon: <MdDelete />,
@@ -35,7 +33,7 @@ export function useFriends() {
         onClick: openRemoveFriendConfirmModal,
       },
     ],
-    [t, startChatOnClick, prepareChatOnHover, openRemoveFriendConfirmModal]
+    [t, openRemoveFriendConfirmModal]
   );
 
   return { friends, loading, error, actions, refresh };
