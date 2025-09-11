684471a6f8b9e82586eae2d7f1ee0165ef555cd3
Alphenx <adriangarcia.comunicacion@gmail.com>
2025-09-11 20:44:11 +0200

    Creation of `AddFriend` action and related components: `AddFriendActionButton`, `AddFriendConfirmModal` and `AddFriendForm`

---
+++ src/features/friends/components/AddFriend/AddFriendConfirmModal.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Modal } from '@/components';
+import { ConfirmModalRenderProps } from '@/features/common/hooks/useConfirmModal';
+import AddFriendForm from '@/features/friends/components/AddFriend/AddFriendForm';
+import { useTranslations } from '@/features/i18n/hooks/useTranslations';
+
+type AddFriendConfirmModalProps = ConfirmModalRenderProps<string>;
+
+export function AddFriendConfirmModal({ onConfirm, ...props }: AddFriendConfirmModalProps) {
+  const { t } = useTranslations('friends', 'friendsRequestList', 'actions', 'sendFriendRequest');
+
+  return <Modal {...props} title={t('Add Friend', 'title')} body={<AddFriendForm onConfirm={onConfirm} t={t} />} />;
+}
+
+export default AddFriendConfirmModal;
